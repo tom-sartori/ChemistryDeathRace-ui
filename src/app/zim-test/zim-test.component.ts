f84007dffa7dcc04b1519ff54eef7394882387eb
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2023-04-25 17:34:34 +0200

    Game responsive.

--- src/app/zim-test/zim-test.component.ts
+++ src/app/zim-test/zim-test.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from "@classes/game";
-import { backgroundColor, pawnColors, pawnRadius, } from "@constants/ui-constants";
-import { Player } from '@classes/player';
-import { Pawn } from '@classes/pawn';
+import { backgroundColor, } from "@constants/ui-constants";
 import { ParamsService } from '@services/params.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-zim-test',
@@ -12,33 +11,33 @@ import { ParamsService } from '@services/params.service';
 })
 export class ZimTestComponent implements OnInit {
 
-  private readonly players: Player[];
-  private readonly numberOfPlayers: number;
-  private readonly difficulty: string;
-  private readonly diceSize: number;
+  constructor(
+    private paramsService: ParamsService,
+    private router: Router
+  ) {
 
-  constructor(private paramsService: ParamsService) {
-    this.numberOfPlayers = paramsService.playersNumber;
-    let players: Player[] = [];
-    for (let i: number = 0; i < this.numberOfPlayers; i++) {
-      players.push(new Player(paramsService.playersName[ i ], new Pawn(pawnRadius, pawnColors[ i ])));
+    if (this.router.url === '/game-test') {
+      new Frame({
+        scaling: FULL,
+        color: backgroundColor,
+        outerColor: backgroundColor,
+        ready: (): void => {
+          new Game(['Jean', 'Michel', 'Denis', 'Henry'], 'S7', 6);
+        }
+      });
     }
-    this.players = players;
-    this.difficulty = paramsService.difficulty;
-    this.diceSize = paramsService.diceSize;
-
-    new Frame({
-      scaling: FULL,
-      color: backgroundColor,
-      outerColor: backgroundColor,
-      ready: this.ready.bind(this)
-    })
+    else {
+      new Frame({
+        scaling: FULL,
+        color: backgroundColor,
+        outerColor: backgroundColor,
+        ready: (): void => {
+          new Game(paramsService.playerNames, paramsService.difficulty, paramsService.diceSize);
+        }
+      });
+    }
   }
 
   ngOnInit(): void {
-  }
-
-  private ready(): void {
-    new Game(this.players, this.difficulty, this.diceSize);
   }
 }
