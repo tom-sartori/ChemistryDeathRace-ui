da6c1ab87c5a32754150da57b55167dcadb48167
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2023-05-16 14:22:37 +0200

    Rename project.

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/chemical-pursuit-ui'),
+      dir: require('path').join(__dirname, './coverage/chemistry-death-race-ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
